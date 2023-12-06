d532febd148bdb765a0d7772703cb7d7c3f01ced
MatusKovac <matus.kovac@kosickaakademia.sk>
2023-12-06 10:00:05 +0100

    edited home+login

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css'],
 })
